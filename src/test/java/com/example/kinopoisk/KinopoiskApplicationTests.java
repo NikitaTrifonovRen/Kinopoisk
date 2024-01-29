5f9942efe3128790e5c09554fcd7ab4e3790d623
NikitaTrifonovRen <SpecNas06@yandex.ru>
2024-01-29 21:46:49 +0500

    With Test

--- src/test/java/com/example/kinopoisk/KinopoiskApplicationTests.java
+++ src/test/java/com/example/kinopoisk/KinopoiskApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.kinopoisk;
 
+import com.example.kinopoisk.controller.FilmsRestController;
+import com.example.kinopoisk.model.FilmSearch;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
 class KinopoiskApplicationTests {
 
+    @Autowired
+    private FilmsRestController filmsRestController;
+    FilmSearch filmSearch = new FilmSearch();
+
+
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(filmsRestController).isNotNull();
     }
+    @Test
+    public void restTestDb() throws Exception{
+        filmSearch.setKeyword("зел");
+        assertThat(filmsRestController.showFilmInDb(filmSearch)).isNotEmpty();
+    }
+    @Test
+    public void restTestResponse() throws Exception{
+        filmSearch.setPage(1);
+        assertThat(filmsRestController.getFilms(filmSearch)).isNotEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+
 
 }
